3e3ddc7a40610910ea10e78687facbf94fcf5c45
applikasiku <amelnewsid@gmail.com>
2026-09-22 00:38:55 +0700

    fix(pwa): refresh sidebar feature assets

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='vidgen-ui-polish-20260922';
+const CACHE='vidgen-sidebar-features-20260922';
 const CORE=['/','/styles.css','/app.js','/manifest.webmanifest','/icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(Promise.all([caches.open(CACHE).then(c=>c.addAll(CORE)),self.skipWaiting()])));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
