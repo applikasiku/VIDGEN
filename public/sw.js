928b9244a991035fbfb07ec98f0e0332be44e6dc
applikasiku <amelnewsid@gmail.com>
2026-09-22 00:56:44 +0700

    release: refresh PWA cache for 1.2.0

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='vidgen-v1.1.0-shell';
+const CACHE='vidgen-v1.2.0-shell';
 const CORE=['/','/styles.css','/app.js','/manifest.webmanifest','/icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(Promise.all([caches.open(CACHE).then(c=>c.addAll(CORE)),self.skipWaiting()])));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
